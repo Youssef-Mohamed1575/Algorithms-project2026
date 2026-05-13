1c2432f4daf6fdfe60f652825a13320b800812dd
youssef_tag <yousseftag50@gmail.com>
2026-05-13 22:17:15 +0300

    added a bubbleSort function

--- triangle.py
+++ triangle.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 class Solution:
+    def Bubblesort(self , arr: List[int]):
+        n = len(arr)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    swapped = True
+            if not swapped:
+                break
+        return arr
+
+
     def checkCondition(self , p1: int , p2: int , p3: int):
         if ((p1 + p2 > p3) and (p1 + p3 > p2) and (p2 + p3 > p1)):
             return True
@@ -10,18 +23,17 @@ class Solution:
 
     # right now this works but its complixty is O(n^3) need a better algorithm
     def triangleBruteForce(self , nums: List[int]) -> int:
-        counter = 0
         for i in range(len(nums) - 2):
             for j in range(i + 1, len(nums) - 1):
                 for k in range(j + 1, len(nums)):
                     if(self.checkCondition(nums[i] , nums[j] , nums[k])):
-                        counter += 1
-        return counter
+                        return 1
+        return 0
                     
 
     def triangleNumber(self, nums: List[int]) -> int:
         ret = 0 
-        nums.sort()
+        nums = self.Bubblesort(nums)
         for k in range(len(nums) - 1, 1, -1):
             i = 0
             j = k - 1
